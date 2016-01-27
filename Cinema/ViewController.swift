15eed44025d55b5732e7a8671ca2ac1fd65a28bf
pdrotrs <patlleon@aol.com>
2016-01-26 23:17:11 -0700

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Pedro A. Torres Leon on 1/25/16.
+//  Copyright © 2016 Torres Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
